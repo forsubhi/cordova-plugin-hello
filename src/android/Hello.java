b34b73d45488d582524e4433b4d92ee75862143f
Mohammed Subhi <subhi.quroush@imona.com>
2016-05-20 17:16:25 +0300

    Update Hello.java

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -1,35 +1,43 @@
 package com.example.plugin;
 
-import org.apache.cordova.*;
+
 import org.json.JSONArray;
 import org.json.JSONException;
+
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import 	android.app.Activity;
+public class Hello extends Activity {
 
-public class Hello extends CordovaPlugin {
+    public  static  int success =10;
+    private CallbackContext callbackContext;
 
-    @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
 
         if (action.equals("greet")) {
 
             String name = data.getString(0);
             String message = "Hi, " + name;
-            
             Activity activity = cordova.getActivity();
             PackageManager pm = activity.getPackageManager();
             Intent launchIntent = pm.getLaunchIntentForPackage("com.kuzufab");
-            activity.startActivity(launchIntent);
-            
-         //   callbackContext.success(message);
+            activity.startActivityForResult(launchIntent,success);
+            this.callbackContext=callbackContext;
+            //   callbackContext.success(message);
 
             return true;
 
         } else {
-            
+
             return false;
 
         }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackContext.success("Hi Plugin");
     }
 }
